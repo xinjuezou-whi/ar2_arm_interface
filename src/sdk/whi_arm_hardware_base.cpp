460f2d926ff9878ed6480cc2a7cbc18a96b621bc
Xinjue Zou <hit_zou@126.com>
2022-12-15 14:43:42 +0800

    remove unnecessary comments

--- src/sdk/whi_arm_hardware_base.cpp
+++ src/sdk/whi_arm_hardware_base.cpp
@@ -13,9 +13,7 @@ All text above must be included in any redistribution.
 
 ******************************************************************/
 #include "ar2_arm_interface/whi_arm_hardware_base.h"
-/// <summary>
-/// 
-/// </summary>
+
 namespace whi_arm_hardware_interface
 {
 	const char* ArmHardware::hardware[HARDWARE_SUM] = { "i2c", "canbus", "serial", "rosserial"};
